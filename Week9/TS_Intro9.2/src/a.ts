9567f9ddd67444f9f87ce56e9a408b7e8eb61cb3
CoderXYZ14 <s1404islam@gmail.com>
2024-02-02 02:08:04 +0530

    Week 9.2-3

--- Week9/TS_Intro9.2/src/a.ts
+++ Week9/TS_Intro9.2/src/a.ts
@@ -35,3 +35,85 @@ function runAfter1S(fn:()=>void){
 runAfter1S(()=>{
     console.log("Hello World");
 });
+
+//interfaces
+interface User{
+    firstName:String;
+    lastName:String;
+};
+
+function Name(user:User){
+    console.log(user.firstName);
+}
+
+Name({
+    firstName:"Shahwaiz",
+    lastName:"Islam"
+});
+
+//Interface 2
+interface Person{
+    name:string;
+    greet(phrase:string):void;
+}
+
+class Employe implements Person{
+    name:string;
+    constructor(n:string){
+        this.name=n;
+    }
+    greet(phrase:string){
+        console.log(`${phrase} ${this.name}`);
+    }
+}
+
+const e=new Employe("Shahwaiz");
+console.log(e.greet("Hi there "));
+
+
+//Type
+// type User={
+//     firstName:string;
+//     lastName:string;
+//     age:number;
+// }
+
+//Union first use of type
+type StringOrNumber=string |number;
+
+function printId(id:StringOrNumber){
+    console.log(`ID:${id}`);
+}
+printId(101);
+printId("123d");
+
+//Intersection second use of type
+type Employee={
+    name:string;
+    startDate:Date;
+};
+type Manager={
+    name:string;
+    department:string;
+};
+type TeamLead=Employee & Manager;
+ let teamLead:TeamLead={
+    name:"Shahwaiz",
+    startDate:new Date(),
+    department:"Web"
+ };
+ console.log(teamLead.name,"",teamLead.startDate,"",teamLead.department);
+
+
+ //Array to return the max value
+
+function maxValue(arr:number[]):number{
+    let maxVal=0;
+    for(let i=0;i<arr.length;i++){
+        if(arr[i]>maxVal)
+            maxVal=arr[i];
+    }
+    return maxVal
+}
+let arr:number[]=[1,2,3,7,6,4,9,4];
+console.log("Max no is ",maxValue(arr));
