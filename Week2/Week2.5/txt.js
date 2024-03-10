6c96cb4b41402eb2b7427beae175ed3b4f4fe7c5
CoderXYZ14 <s1404islam@gmail.com>
2024-03-11 03:33:05 +0530

    Week2.5-2

---
+++ Week2/Week2.5/txt.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const fs = require("fs");
+const app = express();
+
+app.get("/files/:fileName", (req, res) => {
+  const name = req.params.fileName;
+  console.log(name);
+  fs.readFile(name, "utf8", (err, data) => {
+    res.json({ data });
+  });
+});
+app.listen(3000);
